9853a2fa052fa2667831d228384274c9eceb76ef
Vodka_Crew <manbrk@users.noreply.github.com>
2014-08-31 19:46:18 +0300

    немножко переделанный шаблон

--- microsocial/settings.py
+++ microsocial/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
