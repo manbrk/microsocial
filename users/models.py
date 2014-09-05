ab6a9cc86ae9ad595386ef8680df90687a73e1d5
Vodka_Crew <manbrk@users.noreply.github.com>
2014-09-05 22:19:26 +0300

    исправлен RegistrationConfirmView

--- users/models.py
+++ users/models.py
@@ -75,7 +75,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def send_registration_email(self):
         url = 'http://%s%s' % (
             Site.objects.get_current().domain,
-            reverse('registration_confirm', kwargs={'token': Signer(salt='registration_confirm').sign(self.pk)})
+            reverse('registration_confirm', kwargs={'token': Signer(salt='registration-confirm').sign(self.pk)})
         )
         self.email_user(
             ugettext(u'Подтвердите регистрацию на microsocial'),
