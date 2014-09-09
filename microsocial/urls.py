15bfb6e759812cced265fd28cb6a56c959445144
Vodka_Crew <manbrk@users.noreply.github.com>
2014-09-09 21:34:44 +0300

    картинки показываются по своим ссылкам

--- microsocial/urls.py
+++ microsocial/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
@@ -9,5 +10,14 @@ urlpatterns = patterns('',
     url(r'^i18n/setlang/$', csrf_exempt(set_language), name='set_language'),
     url(r'', include('users.urls')),
     url(r'', include('usersauth.urls')),
-    url(r'', include('django.contrib.flatpages.urls')),
 )
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += [
+    url(r'', include('django.contrib.flatpages.urls')),
+]
