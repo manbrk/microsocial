98bcb279524bfd3a5adb7efe4410836c85d2c25e
Vodka_Crew <manbrk@users.noreply.github.com>
2015-02-18 21:24:08 +0200

    отображаются диалоги и сообщения в них

--- dialogs/views.py
+++ dialogs/views.py
@@ -1,6 +1,9 @@
 # coding=utf-8
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, PageNotAnInteger
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 from dialogs.models import Dialog
@@ -11,6 +14,15 @@ class DialogView(TemplateView):
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
+        self.opponnent = None
+        self.dialog = None
+        self.form = None
+        if 'user_id' in kwargs:
+            self.opponnent = get_object_or_404(get_user_model(), pk=kwargs['user_id'])
+            self.dialog = Dialog.objects.get_or_create(request.user, self.opponnent)
+            if not self.dialog:
+                raise Http404
+            # self.form =
         return super(DialogView, self).dispatch(request, *args, **kwargs)
 
     def get_dialogs(self):
@@ -27,7 +39,22 @@ class DialogView(TemplateView):
             items = paginator.page(paginator.num_pages)
         return items
 
+    def get_messages(self):
+        if not self.dialog:
+            return
+        paginator = Paginator(self.dialog.messages.all(), 20)
+        page = self.request.GET.get('messages_page')
+        try:
+            items = paginator.page(page)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        except EmptyPage:
+            items = paginator.page(paginator.num_pages)
+        return items
+
     def get_context_data(self, **kwargs):
         context = super(DialogView, self).get_context_data(**kwargs)
         context['dialogs'] = self.get_dialogs()
+        context['opponent'] = self.opponnent
+        context['dialog_messages'] = self.get_messages()
         return context
